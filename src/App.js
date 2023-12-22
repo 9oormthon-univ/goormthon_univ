bbed9bd71d062ba0b7e3bbf1128eeb7fe5e339dd
simeunseo <rkdcjfehowl@naver.com>
2023-12-22 06:24:44 +0000

    init: eslint, prettier 세팅

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
+import { Button } from '@goorm-dev/gds-components';
 import React, { Component } from 'react';
 
 import { ActivityIcon } from '@goorm-dev/gds-icons';
-import { Button } from '@goorm-dev/gds-components';
 
 class App extends Component {
   render() {
     return (
       <>
-      <ActivityIcon/>
-      <Button>hi</Button>
+        <ActivityIcon />
+        <Button>hi</Button>
       </>
     );
   }
