1d366889b7a77b7b551ab7c352082c704e964a83
simeunseo <rkdcjfehowl@naver.com>
2023-12-22 05:23:45 +0000

    init: GDS 기반 세팅

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+import { ActivityIcon } from '@goorm-dev/gds-icons';
+import { Button } from '@goorm-dev/gds-components';
+
+class App extends Component {
+  render() {
+    return (
+      <>
+      <ActivityIcon/>
+      <Button>hi</Button>
+      </>
+    );
+  }
+}
+
+export default App;
