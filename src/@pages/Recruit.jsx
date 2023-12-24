04fb4f4994d17d7148dea255dfeda900e7c88692
seochan99 <gmlcks0513@naver.com>
2023-12-24 03:51:02 +0000

    feat : github연동 테스트

--- src/@pages/Recruit.jsx
+++ src/@pages/Recruit.jsx
@@ -1,9 +1,14 @@
 import React from 'react';
+import S from './About.module.css';
+import { Button } from 'reactstrap';
 
 export default function Recruit() {
   return (
     <>
-      <span>Recruit</span>
+      <span>
+	  모집 페이지 입니다.
+		  <Button>신청하기</Button>
+	  </span>
     </>
   );
 }
