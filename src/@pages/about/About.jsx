067388779c518a4aa8224f3711e935abfdb7da0b
seochan99 <gmlcks0513@naver.com>
2023-12-26 03:19:13 +0000

    style : goorm strap 적용

--- src/@pages/about/About.jsx
+++ src/@pages/about/About.jsx
@@ -1,9 +1,17 @@
+import { GoormNavbar } from '@goorm-dev/gds-components';
 import React from 'react';
 import * as S from './style';
 
 export default function About() {
   return (
     <>
+      <h1>아아</h1>
+      <h2>아아</h2>
+      <h3>아아</h3>
+      <h4>아아</h4>
+      <h5>아아</h5>
+      <a href="#">구름 a태그가 맞나?</a>
+      <h6>은?</h6>
       <S.MainWrapper>ㅇ응아아아ㅏㄱㄱ</S.MainWrapper>
     </>
   );
--- src/App.js
+++ src/App.js
@@ -9,10 +9,10 @@ class App extends Component {
   render() {
     return (
       <>
-        <ThemeProvider theme={theme}>
-          <GlobalStyle />
-          <Router />
-        </ThemeProvider>
+        {/* <ThemeProvider theme={theme}>
+          <GlobalStyle /> */}
+        <Router />
+        {/* </ThemeProvider> */}
       </>
     );
   }
