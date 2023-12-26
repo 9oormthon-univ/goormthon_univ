3f19bebf6f039cfe7f4865e612eec22284dadee1
seochan99 <gmlcks0513@naver.com>
2023-12-26 02:05:39 +0000

    feat: router lazy적용

--- src/Router.jsx
+++ src/Router.jsx
@@ -1,17 +1,20 @@
-import React from 'react';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import About from './@pages/About';
-// import Project from './@pages/Project';
-import Recruit from './@pages/Recruit';
+import React, { lazy, Suspense } from 'react';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+
+// 레이지 로딩 적용
+const About = lazy(() => import('./@pages/About'));
+const Project = lazy(() => import('./@pages/Project'));
+const Recruit = lazy(() => import('./@pages/Recruit'));
+const NotFound = lazy(() => import('./@pages/errors/NotFound'));
+
+// 라우터 생성
+const router = createBrowserRouter([
+  { path: '/', element: <Suspense fallback={<div>Loading...</div>}>{React.createElement(About)}</Suspense> },
+  { path: '/project', element: <Suspense fallback={<div>Loading...</div>}>{React.createElement(Project)}</Suspense> },
+  { path: '/recruit', element: <Suspense fallback={<div>Loading...</div>}>{React.createElement(Recruit)}</Suspense> },
+  { path: '*', element: <Suspense fallback={<div>Loading...</div>}>{React.createElement(NotFound)}</Suspense> },
+]);
 
 export default function Router() {
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<About />} />
-        {/* <Route path="/project" element={<Project />} /> */}
-        <Route path="/recruit" element={<Recruit />} />
-      </Routes>
-    </BrowserRouter>
-  );
+  return <RouterProvider router={router}></RouterProvider>;
 }
