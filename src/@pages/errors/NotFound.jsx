d1158680b21f9587c89591d1c2a1ea9b3196cd33
seochan99 <gmlcks0513@naver.com>
2023-12-26 02:06:28 +0000

    feat: notfound page deafult message

--- src/@pages/errors/NotFound.jsx
+++ src/@pages/errors/NotFound.jsx
@@ -1,14 +1,25 @@
+import React from 'react';
 import { useRouteError } from 'react-router-dom';
 import * as S from './style';
 
 export default function NotFound() {
   const error = useRouteError();
 
+  // 디폴트 메세지
+  let errorMessage = "The page you're looking for cannot be found.";
+  let errorDetail = 'Please check the URL or go back to the homepage.';
+
+  // 에러 있을시
+  if (error) {
+    errorMessage = error.statusText || 'An unexpected error has occurred.';
+    errorDetail = error.message || '';
+  }
+
   return (
     <S.MainWrapper>
       <S.MainTitle>Oops!</S.MainTitle>
-      <S.MainDescription>Sorry, an unexpected error has occurred</S.MainDescription>
-      <S.MainSubDescription>{error.statusText || error.message}</S.MainSubDescription>
+      <S.MainDescription>{errorMessage}</S.MainDescription>
+      <S.MainSubDescription>{errorDetail}</S.MainSubDescription>
     </S.MainWrapper>
   );
 }
