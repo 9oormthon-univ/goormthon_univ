6e6ae43beeda882a43b917f4aa79e30efdd88c41
seochan99 <gmlcks0513@naver.com>
2023-12-27 08:09:19 +0000

    feat: notfound 제작

--- src/@pages/errors/NotFound.jsx
+++ src/@pages/errors/NotFound.jsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import { useRouteError } from 'react-router-dom';
+import NotFoundImg from '../../assets/images/notfound.png';
 import * as S from './style';
 
 export default function NotFound() {
   const error = useRouteError();
 
   // 디폴트 메세지
-  let errorMessage = "The page you're looking for cannot be found.";
-  let errorDetail = 'Please check the URL or go back to the homepage.';
+  let errorMessage = '이런, 잘못된 페이지로 들어왔네요! 상단 오른쪽 로고를 통해 메인으로 이동해보아요 :) ';
+  let errorDetail = '이런, 잘못된 페이지로 들어왔네요! 상단 오른쪽 로고를 통해 메인으로 이동해보아요 :)';
 
   // 에러 있을시
   if (error) {
@@ -17,8 +18,9 @@ export default function NotFound() {
 
   return (
     <S.MainWrapper>
-      <S.MainTitle>Oops!</S.MainTitle>
-      <S.MainDescription>{errorMessage}</S.MainDescription>
+      <S.NotFoundImg src={NotFoundImg} />
+      <S.MainTitle>잘못된 페이지에요!</S.MainTitle>
+      {/* <S.MainDescription>{errorMessage}</S.MainDescription> */}
       <S.MainSubDescription>{errorDetail}</S.MainSubDescription>
     </S.MainWrapper>
   );
