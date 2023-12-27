ecad692024ddd8e777ad1b8833bbe95950fe2e10
seochan99 <gmlcks0513@naver.com>
2023-12-27 04:51:37 +0000

    refactor : 모집대상 리팩토링

--- src/@pages/recruit/Recruit.jsx
+++ src/@pages/recruit/Recruit.jsx
@@ -1,37 +1,19 @@
 import React from 'react';
 import * as S from './style';
 import RecuritHeader from '../../@components/recruit/recuritHeader/RecuritHeader';
-import { CalendarIcon, GroupIcon, SchoolIcon } from '@goorm-dev/gds-icons';
+import RecuritTarget from '../../@components/recruit/recuritTarget/RecuritTarget';
 
 export default function Recruit() {
   return (
     <>
       <RecuritHeader />
       <S.RecuritBody>
-        <S.TargetContainer>
-          <S.TargetTitle>학교 대표 지원 대상</S.TargetTitle>
-          <S.TargetDetail>: IT 서비스에 관심이 많고 교내 IT 확산을 하고자 하는 사람</S.TargetDetail>
-          <S.TargetRowDetailWrapper>
-            <S.TargetRowDetailBox>
-              {/* icon */}
-              <SchoolIcon width="2rem" className="SchoolIcon__icon" color="black" />
-              <S.TargetDetailBoxText>대학 재학 및 휴학중인 사람</S.TargetDetailBoxText>
-              {/* text */}
-            </S.TargetRowDetailBox>
-            <S.TargetRowDetailBox>
-              {/* icon */}
-              <CalendarIcon width="2rem" className="CalendarIcon__icon" color="black" />
-              <S.TargetDetailBoxText>2월부터 8월까지 활동이 가능한 사람</S.TargetDetailBoxText>
-              {/* text */}
-            </S.TargetRowDetailBox>
-            <S.TargetRowDetailBox>
-              {/* icon */}
-              <GroupIcon width="2rem" className="GroupIcon__icon" color="black" />
-              <S.TargetDetailBoxText>교내 운영 가능한 사람</S.TargetDetailBoxText>
-              {/* text */}
-            </S.TargetRowDetailBox>
-          </S.TargetRowDetailWrapper>
-        </S.TargetContainer>
+        {/* 지원 대상 */}
+        <RecuritTarget />
+        
+        <S.BodyTitle>
+          모집일정
+        </S.BodyTitle>
       </S.RecuritBody>
     </>
   );
--- src/@pages/recruit/style.jsx
+++ src/@pages/recruit/style.jsx
@@ -7,52 +7,13 @@ export const RecuritBody = styled.div`
   justify-content: start;
   align-items: center;
   border: 1px solid blue;
-`;
-export const TargetContainer = styled.div`
-  width: 100%;
-  flex-direction: column;
-  border: 1px solid red;
-  margin-top: 8.56rem;
-  margin-bottom: 9.06rem;
+  @media (max-width: 768px) {
+    width: 80%;
+  }
 `;
 
-export const TargetTitle = styled.h2`
+export const BodyTitle = styled.h2`
   color: #000;
   font-feature-settings: 'clig' off, 'liga' off;
   margin-bottom: 2.25rem;
-`;
-
-export const TargetDetail = styled.h5`
-  color: var(--light-gray-gray-900, #2b2d36);
-  font-feature-settings: 'clig' off, 'liga' off;
-  margin-bottom: 1.5rem;
-`;
-
-export const TargetRowDetailWrapper = styled.div`
-  display: flex;
-  flex-direction: row;
-  gap: 1rem;
-`;
-
-export const TargetRowDetailBox = styled.div`
-  display: flex;
-  flex-direction: row;
-  align-items: center;
-  justify-content: start;
-  gap: 0.5rem;
-  border-radius: 0.5rem;
-  padding: 0.75rem 1rem;
-  background: var(--light-gray-gray-100, #f0f0f5);
-`;
-
-export const TargetDetailBoxText = styled.p`
-  color: #000;
-  font-feature-settings: 'clig' off, 'liga' off;
-
-  /* KOR/subtitle-1ㅣ0.875rem */
-  font-family: Pretendard;
-  font-size: 0.875rem;
-  font-style: normal;
-  font-weight: 500;
-  margin-bottom: 0;
 `;
