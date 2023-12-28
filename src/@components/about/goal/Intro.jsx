e0378b0e3fe14636b514f6ed8a88ac0d1441363e
simeunseo <rkdcjfehowl@naver.com>
2023-12-28 06:09:18 +0000

    design: about goal 섹션 구현 (애니메이션 제외)

---
+++ src/@components/about/goal/Intro.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import * as S from './style';
+
+import GoalImg from '../../../assets/svgs/goal-graphic.svg';
+import GoalRainImg from '../../../assets/svgs/goal-rain.svg';
+
+export default function Goal() {
+  return (
+    <S.GoalWrapper>
+      <S.HeaderTextWrapper>
+        <S.HeaderDescriptionText>우리의 목표는 단 한 가지</S.HeaderDescriptionText>
+        <S.HeaderTitleText>
+          직접 구현한 서비스가 작은 물방울이 되어
+          <br />큰 구름을 이루는 기회를 제공하는 것
+        </S.HeaderTitleText>
+      </S.HeaderTextWrapper>
+      <S.Img src={GoalImg} />
+      <S.MovingImg src={GoalRainImg} />
+    </S.GoalWrapper>
+  );
+}
---
+++ src/@components/about/goal/style.jsx
@@ -0,0 +1,52 @@
+import styled from 'styled-components';
+
+export const GoalWrapper = styled.div`
+  height: 100vh;
+
+  position: relative;
+  z-index: 1;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  background-color: var(--light-gray-gray-900, #2b2d36);
+
+  padding: 9.44rem;
+`;
+
+export const HeaderTextWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  align-items: start;
+`;
+
+export const HeaderTitleText = styled.h3`
+  color: var(--light-white-white-light, #fff);
+  font-feature-settings: 'clig' off, 'liga' off;
+`;
+
+export const HeaderDescriptionText = styled.h6`
+  color: var(--light-white-white-light, #fff);
+  font-feature-settings: 'clig' off, 'liga' off;
+`;
+
+export const Img = styled.img`
+  width: max-content;
+  height: max-content;
+
+  position: absolute;
+  bottom: 0;
+  right: 4.69rem;
+
+  z-index: 1;
+`;
+
+export const MovingImg = styled.img`
+  width: max-content;
+  height: max-content;
+
+  position: absolute;
+  bottom: 0;
+  right: 25.62rem;
+`;
