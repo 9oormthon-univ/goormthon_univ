a7f0524b7b7373ae0d38b5713cb85f5b6893c305
simeunseo <rkdcjfehowl@naver.com>
2023-12-29 04:39:18 +0000

    docs: 9oormthon univ -> 9oormthonUNIV 변경

--- src/@components/about/benefit/Benefit.jsx
+++ src/@components/about/benefit/Benefit.jsx
@@ -8,7 +8,7 @@ export default function Benefit() {
   return (
     <S.BenefitWrapper>
       <GridContainer>
-        <S.HeaderTitleText>구름톤 유니브의 일원이 된다면,</S.HeaderTitleText>
+        <S.HeaderTitleText>9oormthonUNIV의 일원이 된다면,</S.HeaderTitleText>
         <BenefitList />
       </GridContainer>
     </S.BenefitWrapper>
