01c2de78d7e58607f5a05622cdec44619232e6dd
simeunseo <rkdcjfehowl@naver.com>
2024-01-02 07:08:46 +0000

    design: width 꽉 채우기

--- src/@components/about/benefit/BenefitMobile/BenefitItem.jsx
+++ src/@components/about/benefit/BenefitMobile/BenefitItem.jsx
@@ -4,8 +4,8 @@ import * as S from './style';
 
 export default function BenefitItem({ imgSrc, iconSrc, bgColor, title, description, url }) {
   return (
-    <Link to={url} target="\_blank">
-      <S.BenefitItemWrapper>
+    <S.BenefitItemWrapper>
+      <Link to={url} target="\_blank">
         <S.BenefitImgWrapper $bgColor={bgColor}>
           <S.BenefitImg src={imgSrc} />
         </S.BenefitImgWrapper>
@@ -16,7 +16,7 @@ export default function BenefitItem({ imgSrc, iconSrc, bgColor, title, descripti
           </S.BenefitTextWrapper>
           <S.Icon src={iconSrc} />
         </S.BenefitContentsWrapper>
-      </S.BenefitItemWrapper>
-    </Link>
+      </Link>
+    </S.BenefitItemWrapper>
   );
 }
--- src/@components/about/benefit/BenefitMobile/style.jsx
+++ src/@components/about/benefit/BenefitMobile/style.jsx
@@ -18,7 +18,6 @@ export const BenefitWrapper = styled.div`
 export const HeaderTitleText = styled.h2`
   color: var(--light-black-black, #000);
   font-feature-settings: 'clig' off, 'liga' off;
-  width: 100%;
   text-align: start;
   margin-top: 5rem;
 `;
@@ -26,11 +25,16 @@ export const HeaderTitleText = styled.h2`
 export const BenefitItemWrapper = styled.div`
   display: flex;
   flex-direction: column;
+  width: 100%;
+
+  & > a {
+    width: 100%;
+  }
 `;
 
 export const BenefitImgWrapper = styled.div`
   background-color: ${({ $bgColor }) => $bgColor};
-  width: 19rem;
+  width: 100%;
   height: 13.6875rem;
   border-radius: 1rem 1rem 0rem 0rem;
   display: flex;
@@ -44,10 +48,10 @@ export const BenefitImg = styled.img`
 `;
 
 export const BenefitContentsWrapper = styled.div`
+  width: 100%;
   background-color: var(--light-white-white-light, #fff);
   border-radius: 0rem 0rem var(--Spacing-space-500, 1rem) var(--Spacing-space-500, 1rem);
   border: 1px solid var(--light-gray-gray-300, #e1e1e8);
-  width: 19rem;
   display: flex;
   justify-content: space-between;
   align-items: end;
@@ -55,6 +59,7 @@ export const BenefitContentsWrapper = styled.div`
 `;
 
 export const BenefitTextWrapper = styled.div`
+  width: 100%;
   display: flex;
   flex-direction: column;
   gap: 0.5rem;
