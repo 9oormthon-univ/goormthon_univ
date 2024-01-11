2532a0e0403a982b0a90f8d3769be4d43dadf35a
simeunseo <rkdcjfehowl@naver.com>
2024-01-11 08:09:41 +0000

    design: activity 반응형 완료

--- src/@components/about/activity/style.jsx
+++ src/@components/about/activity/style.jsx
@@ -18,55 +18,112 @@ export const HeaderTitleText = styled.h2`
   width: 100%;
   text-align: center;
   margin-top: 10.19rem;
-  @media screen and (max-width: 479px) {
-    margin-top: 5.81rem;
+
+  /* container-xs, s, md */
+  @media screen and (min-width: 0px) {
+    display: none;
+  }
+
+  /* container-xl */
+  @media screen and (min-width: 1200px) {
+    display: block;
+  }
+`;
+
+export const HeaderTitleTextSmall = styled.h3`
+  width: 100%;
+  text-align: center;
+  margin-top: 10.19rem;
+
+  /* container-xs, s, md */
+  @media screen and (min-width: 0px) {
+    display: block;
+  }
+
+  /* container-xl */
+  @media screen and (min-width: 1200px) {
+    display: none;
   }
 `;
 
 export const ActivityItemWrapper = styled.div`
   display: flex;
   gap: 1rem;
+  height: 100%;
 
-  /* flex-flow: ${({ $idx }) => ($idx === 2 ? 'unset' : '')}; */
-    flex-direction: column;
-    flex-flow: column-reverse;
-  /* @media screen and (max-width: 1278px) {
-    flex-direction: column;
-    flex-flow: column-reverse;
-  } */
+  flex-direction: column;
+  flex-flow: column-reverse;
+
+  @media screen and (min-width: 0px) {
+    & img {
+      height: ${({ $idx }) => $idx == 2 && '12.125rem'};
+    }
+  }
+
+  @media screen and (min-width: 1200px) {
+    & img {
+      height: ${({ $idx }) => $idx == 2 && '20rem'};
+    }
+  }
 `;
 
 export const ActivityListWrapper = styled.div`
-  display: grid;
-  grid-template-columns: 1fr 1fr;
-  gap: 1.5rem;
-  width: 100%;
-  margin-top: 3.94rem;
-  margin-bottom: 14.81rem;
-
-  /* Adjust the gap as needed for responsiveness */
-  @media screen and (max-width: 768px) {
+  /* container-xs */
+  @media screen and (min-width: 0px) {
+    width: 20.5rem;
     display: flex;
     flex-direction: column;
 
-    gap: 3.94rem;
+    gap: 2rem;
   }
 
-  @media screen and (max-width: 479px) {
-    margin-bottom: 5.81rem;
+  /* container-md */
+  @media screen and (min-width: 768px) {
+    width: 43rem;
+    display: grid;
+    grid-template-areas:
+      'activity1 activity2'
+      'activity3 activity3'
+      'activity4 activity5';
+
+    grid-template-columns: 20.5rem 20.5rem;
+    grid-template-rows: 20.25rem 18.625rem 20.25rem;
+
+    &::nth-child(1) {
+      grid-area: activity1;
+    }
+    &::nth-child(2) {
+      grid-area: activity2;
+    }
+    &::nth-child(3) {
+      grid-area: activity3;
+    }
+    &::nth-child(4) {
+      grid-area: activity4;
+    }
+    &::nth-child(5) {
+      grid-area: activity5;
+    }
   }
+
+  /* container-xl */
+  @media screen and (min-width: 1200px) {
+    width: 71.125rem;
+    justify-content: center;
+
+    grid-template-columns: 34.5625rem 34.5625rem;
+    grid-template-rows: 28.25rem 26.75rem 28.25rem;
+  }
+
+  margin-top: 3.94rem;
+  margin-bottom: 14.81rem;
 `;
 export const ContentBox = styled.article`
-  border-radius: 0.5rem;
   display: flex;
   flex-direction: column;
 
   height: 8.75rem;
   min-width: 21rem;
-
-  /* @media screen and (max-width: 1278px) {
-    height: 100%;
-  } */
 `;
 
 export const ContentTitleWrapper = styled.div`
@@ -88,11 +145,10 @@ export const ContentDescriptionText = styled.p`
 `;
 
 export const ContentImg = styled.img`
-  width: 100%;
-  max-width: 100%;
-  /* @media screen and (max-width: 1278px) {
-    width: 100%;
-    max-width: 100%;
+  object-fit: cover;
+
+  /* container-xs */
+  @media screen and (min-width: 0px) {
+    border-radius: 1rem;
   }
-  max-width: 30.75rem; */
 `;
