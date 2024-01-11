4b709ff0a8e9cad72decafb0f3ca9039e36b8804
simeunseo <rkdcjfehowl@naver.com>
2024-01-11 01:38:36 +0000

    design: fillbar height 수정

--- src/@components/about/plan/planDesktop/style.jsx
+++ src/@components/about/plan/planDesktop/style.jsx
@@ -80,7 +80,7 @@ export const TimelineBar = styled.figure`
 
 export const TimelineFillBar = styled.div`
   width: ${({ $fillRatio }) => $fillRatio}%;
-  height: var(--size-500);
+  height: 2.75rem;
   background-color: var(--blue-500);
   border-radius: 1.34375rem;
   position: absolute;
