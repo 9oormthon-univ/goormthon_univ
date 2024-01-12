6f0fb97da1568eec7443c01e40fb887bcb2af435
simeunseo <rkdcjfehowl@naver.com>
2024-01-12 06:38:41 +0000

    style: mainBanner goormstrap 적용

--- src/@components/about/mainBanner/MainBanner.jsx
+++ src/@components/about/mainBanner/MainBanner.jsx
@@ -4,15 +4,17 @@ import MainBannerSlogan from '../../../assets/svgs/main_banner_slogan.svg';
 
 export default function MainBanner({ scrollTrigger }) {
   return (
-    <S.MainBannerWrapper className="-container-xl-">
-      <S.HeaderTextWrapper>
-        <S.HeaderDescriptionText>사계절, 구름과 함께</S.HeaderDescriptionText>
-        <S.HeaderDescriptionTextSmall>사계절, 구름과 함께</S.HeaderDescriptionTextSmall>
+    <S.MainBannerWrapper className="d-flex flex-column justify-content-between align-items-center position-fixed w-100">
+      <S.HeaderTextWrapper className="d-flex flex-column">
+        <S.HeaderDescriptionText className="d-none d-md-block text-center">사계절, 구름과 함께</S.HeaderDescriptionText>
+        <S.HeaderDescriptionTextSmall className="d-block d-md-none text-center">
+          사계절, 구름과 함께
+        </S.HeaderDescriptionTextSmall>
         <S.HeaderTitle src={MainBannerSlogan} />
       </S.HeaderTextWrapper>
-      <S.MainImgWrapper>
-        <S.MainImg onClick={scrollTrigger} />
-        <S.MainSmallImg onClick={scrollTrigger} />
+      <S.MainImgWrapper className="p-relative">
+        <S.MainImg onClick={scrollTrigger} className="d-none d-sm-block position-absolute" />
+        <S.MainSmallImg onClick={scrollTrigger} className="d-block d-sm-none position-absolute" />
       </S.MainImgWrapper>
     </S.MainBannerWrapper>
   );
--- src/@components/about/mainBanner/style.jsx
+++ src/@components/about/mainBanner/style.jsx
@@ -9,19 +9,10 @@ export const MainBannerWrapper = styled.div`
     height: 100dvh;
   }
 
-  width: 100%;
-  display: flex;
-  flex-direction: column;
-  justify-content: space-between;
-  align-items: center;
-  position: fixed;
   background-color: var(--gray-950);
 `;
 
 export const HeaderTextWrapper = styled.div`
-  display: flex;
-  flex-direction: column;
-
   /* container-xs */
   @media screen and (min-width: 0px) {
     margin-top: 8.5rem;
@@ -62,40 +53,14 @@ export const HeaderTitle = styled.img`
 `;
 
 export const HeaderDescriptionText = styled.h3`
-  text-align: center;
   color: var(--gray-300);
-
-  /* container-xs */
-  @media screen and (min-width: 0px) {
-    display: none;
-  }
-
-  /* container-md, xl */
-  @media screen and (min-width: 768px) {
-    display: block;
-  }
 `;
 
 export const HeaderDescriptionTextSmall = styled.h5`
-  text-align: center;
   color: var(--gray-300);
-
-  /* container-xs */
-  @media screen and (min-width: 0px) {
-    display: block;
-  }
-
-  /* container-md, xl */
-  @media screen and (min-width: 768px) {
-    display: none;
-  }
 `;
 
 export const MainImgWrapper = styled.div`
-  position: relative;
-  width: 90%;
-  height: 17.25rem;
-
   cursor: pointer;
 
   & > svg {
@@ -103,41 +68,23 @@ export const MainImgWrapper = styled.div`
   }
   &:hover > svg {
     stroke-dashoffset: 0;
-    & > path#click {
-      fill: black;
-    }
-  }
-
-  @media screen and (max-width: 379px) {
-    width: 80%;
   }
 `;
 
 export const MainImg = styled(MainBannerImg)`
-  position: absolute;
   bottom: 6.75rem;
   left: 50%;
   transform: translate(-50%);
 
   stroke-dasharray: 500;
   transition: stroke-dashoffset 0.5s ease-in-out;
-
-  @media screen and (max-width: 576px) {
-    display: none;
-  }
 `;
 
 export const MainSmallImg = styled(MainBannerSmallImg)`
-  display: none;
-  position: absolute;
   bottom: 6.5rem;
   left: 50%;
   transform: translate(-50%);
 
   stroke-dasharray: 500;
   transition: stroke-dashoffset 0.5s ease-in-out;
-
-  @media screen and (max-width: 576px) {
-    display: block;
-  }
 `;
