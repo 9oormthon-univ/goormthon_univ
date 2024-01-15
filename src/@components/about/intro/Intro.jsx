a841554c6267995ab51c5a1b389e7d6f1d0f4e83
simeunseo <rkdcjfehowl@naver.com>
2024-01-15 06:13:37 +0000

    style: intro scss 마이그레이션

--- src/@components/about/intro/Intro.jsx
+++ src/@components/about/intro/Intro.jsx
@@ -1,5 +1,7 @@
-import React, { useState } from 'react';
-import * as S from './style';
+import React from 'react';
+
+import classNames from 'classnames/bind';
+import styles from './Intro.module.scss';
 
 import BeotkkotImg from '../../../assets/images/about/img-beotkkot-default.png';
 import GoormImg from '../../../assets/images/about/img-goorm-default.png';
@@ -9,47 +11,45 @@ import BeotkkotHoverImg from '../../../assets/images/about/img-beotkkot-hover.pn
 import GoormHoverImg from '../../../assets/images/about/img-goorm-hover.png';
 import DanpoongHoverImg from '../../../assets/images/about/img-danpoong-hover.png';
 
-import MainCloudUpImg from '../../../assets/svgs/main-cloud-up.svg';
+const cx = classNames.bind(styles);
 
 export default function Intro({ scrollTarget }) {
   return (
-    <S.IntroWrapper
+    <div
       ref={scrollTarget}
-      className="h-100 position-relative d-flex flex-column justify-contents-center align-items-center"
+      className={cx('intro', 'h-100 position-relative d-flex flex-column justify-contents-center align-items-center')}
     >
-      <S.MainCloudUpImg url={MainCloudUpImg} className="w-100 position-absolute" />
-      <S.HeaderTitleText className="d-none d-md-block text-align-center mt-4">9oormthonUNIV 는</S.HeaderTitleText>
-      <S.HeaderTitleTextSmall className="d-block d-md-none text-align-center mt-4">
-        9oormthonUNIV 는
-      </S.HeaderTitleTextSmall>
-      <S.HeaderTextWrapper className="d-flex flex-column">
-        <S.HeaderDescriptionText className="d-none d-md-block">
+      <div className={cx('cloudUp', 'w-100 position-absolute')} />
+      <h5 className={cx('title', 'd-none d-md-block text-align-center mt-4')}>9oormthonUNIV 는</h5>
+      <h6 className={cx('title', 'd-block d-md-none text-align-center mt-4')}>9oormthonUNIV 는</h6>
+      <div className="d-flex flex-column">
+        <h3 className={cx('description', 'd-none d-md-block')}>
           봄과 가을을 기수로 하여
           <br />
           아이디어 실현의 장을 제공 해주는 IT 연합 동아리 입니다.
-        </S.HeaderDescriptionText>
-        <S.HeaderDescriptionTextSmall className="d-block d-md-none">
+        </h3>
+        <h4 className={cx('description', 'd-block d-md-none')}>
           봄과 가을을 기수로 하여
           <br />
           아이디어 실현의 장을 제공 해주는 IT 연합 동아리 입니다.
-        </S.HeaderDescriptionTextSmall>
-      </S.HeaderTextWrapper>
-      <S.ImgsWrapper className="w-100 d-flex justify-content-center">
-        <S.Imgs>
-          <S.ImgsSetWrapper className="beotkkot position-relative">
-            <S.Img src={BeotkkotImg} className="position-absolute w-100 h-100" />
-            <S.Img src={BeotkkotHoverImg} className="hover-img position-absolute w-100 h-100" />
-          </S.ImgsSetWrapper>
-          <S.ImgsSetWrapper className="goorm position-relative">
-            <S.Img src={GoormImg} className="position-absolute w-100 h-100" />
-            <S.Img src={GoormHoverImg} className="hover-img position-absolute w-100 h-100" />
-          </S.ImgsSetWrapper>
-          <S.ImgsSetWrapper className="danpoong position-relative">
-            <S.Img src={DanpoongImg} className="position-absolute w-100 h-100" />
-            <S.Img src={DanpoongHoverImg} className="hover-img position-absolute w-100 h-100" />
-          </S.ImgsSetWrapper>
-        </S.Imgs>
-      </S.ImgsWrapper>
-    </S.IntroWrapper>
+        </h4>
+      </div>
+      <div className="w-100 d-flex justify-content-center">
+        <figure className={cx('imgs')}>
+          <div className={cx('imgsSet', 'beotkkot', 'position-relative')}>
+            <img src={BeotkkotImg} className={cx('img', 'position-absolute w-100 h-100')} />
+            <img src={BeotkkotHoverImg} className={cx('img', 'hover-img', 'position-absolute w-100 h-100')} />
+          </div>
+          <div className={cx('imgsSet', 'goorm', 'position-relative')}>
+            <img src={GoormImg} className={cx('img', 'position-absolute w-100 h-100')} />
+            <img src={GoormHoverImg} className={cx('img', 'hover-img', 'position-absolute w-100 h-100')} />
+          </div>
+          <div className={cx('imgsSet', 'danpoong', 'position-relative')}>
+            <img src={DanpoongImg} className={cx('img', 'position-absolute w-100 h-100')} />
+            <img src={DanpoongHoverImg} className={cx('img', 'hover-img', 'position-absolute w-100 h-100')} />
+          </div>
+        </figure>
+      </div>
+    </div>
   );
 }
--- src/@components/about/mainBanner/MainBanner.jsx
+++ src/@components/about/mainBanner/MainBanner.jsx
@@ -2,22 +2,26 @@ import React from 'react';
 import MainBannerSlogan from '../../../assets/svgs/main_banner_slogan.svg';
 import MainBannerImg from '../../../assets/svgs/MainBannerImg';
 import MainBannerSmallImg from '../../../assets/svgs/MainBannerSmallImg';
+import classNames from 'classnames/bind';
+import styles from './MainBanner.module.scss';
 
-import './MainBanner.scss';
+const cx = classNames.bind(styles);
 
 export default function MainBanner({ scrollTrigger }) {
   return (
-    <div className="main-banner d-flex flex-column justify-content-between align-items-center position-fixed w-100">
-      <div className="header-text d-flex flex-column">
+    <div
+      className={cx('mainBanner', 'd-flex flex-column justify-content-between align-items-center position-fixed w-100')}
+    >
+      <div className={cx('headerText', 'd-flex flex-column')}>
         <h3 className="d-none d-md-block text-center">사계절, 구름과 함께</h3>
         <h5 className="d-block d-md-none text-center">사계절, 구름과 함께</h5>
-        <img className="header-text__title" src={MainBannerSlogan} />
+        <img className={cx('title')} src={MainBannerSlogan} />
       </div>
       <div>
-        <MainBannerImg onClick={scrollTrigger} className="main-banner__img d-none d-sm-block position-absolute" />
+        <MainBannerImg onClick={scrollTrigger} className={cx('mainImg', 'd-none d-sm-block position-absolute')} />
         <MainBannerSmallImg
           onClick={scrollTrigger}
-          className="main-banner__img__small d-block d-sm-none position-absolute"
+          className={cx('mainImgSmall', 'd-block d-sm-none position-absolute')}
         />
       </div>
     </div>
