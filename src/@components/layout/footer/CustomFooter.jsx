0a732baa108e2211c27899e9683852b28f1cdc86
simeunseo <rkdcjfehowl@naver.com>
2024-01-15 08:59:53 +0000

    design: customFooter 디자인 변경

--- src/@components/layout/footer/CustomFooter.jsx
+++ src/@components/layout/footer/CustomFooter.jsx
@@ -5,22 +5,24 @@ import { FacebookIcon, GithubIcon, InstagramIcon } from '@goorm-dev/gds-icons';
 function CustomFooter() {
   return (
     <S.FooterWrapper>
-      <S.FooterSlogan>Being All Seasons with goorm</S.FooterSlogan>
-      <S.SocialContainer>
-        <S.SocialIcon href="https://github.com/goormthon-Univ" target="_blank" rel="noopener noreferrer">
-          <GithubIcon width="40" height="40" />
-        </S.SocialIcon>
-        <S.SocialIcon
-          href="https://www.instagram.com/9oormthonuniv.official/"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <InstagramIcon width="40" height="40" />
-        </S.SocialIcon>
-        <S.SocialIcon href="https://facebook.com/9oormthonuniv.official/" target="_blank" rel="noopener noreferrer">
-          <FacebookIcon width="40" height="40" />
-        </S.SocialIcon>
-      </S.SocialContainer>
+      <S.FooterContents>
+        <S.FooterSlogan>Being All Seasons with goorm</S.FooterSlogan>
+        <S.SocialContainer>
+          <S.SocialIcon href="https://github.com/goormthon-Univ" target="_blank" rel="noopener noreferrer">
+            <GithubIcon width="40" height="40" />
+          </S.SocialIcon>
+          <S.SocialIcon
+            href="https://www.instagram.com/9oormthonuniv.official/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <InstagramIcon width="40" height="40" />
+          </S.SocialIcon>
+          <S.SocialIcon href="https://facebook.com/9oormthonuniv.official/" target="_blank" rel="noopener noreferrer">
+            <FacebookIcon width="40" height="40" />
+          </S.SocialIcon>
+        </S.SocialContainer>
+      </S.FooterContents>
     </S.FooterWrapper>
   );
 }
--- src/@components/layout/footer/style.jsx
+++ src/@components/layout/footer/style.jsx
@@ -3,14 +3,37 @@ import styled from 'styled-components';
 export const FooterWrapper = styled.div`
   position: relative;
   z-index: 1;
+
+  width: 100%;
+
+  background: var(--gray-000);
+`;
+
+export const FooterContents = styled.div`
+  height: 8rem;
   display: flex;
   flex-direction: row;
   align-items: center;
   justify-content: space-between;
-  width: 100%;
-  height: 8rem;
-  padding: 0.5rem 2rem;
-  background: var(--gray-100);
+  border-top: 1px solid var(--gray-300);
+
+  /* gds footer와 동일한 반응형 레이아웃으로 맞춤 */
+  max-width: 90rem;
+  margin: auto;
+
+  @media (min-width: 1200px) and (max-width: 1439.98px) {
+    max-width: 75rem;
+    padding: 0 1.875rem;
+  }
+
+  @media (min-width: 992px) and (max-width: 1199.98px) {
+    max-width: 62rem;
+    padding: 0 2.625rem;
+  }
+
+  @media (max-width: 991.98px) {
+    padding: 0 1rem;
+  }
 `;
 
 export const SocialContainer = styled.div`
@@ -24,10 +47,10 @@ export const SocialContainer = styled.div`
 `;
 
 export const FooterSlogan = styled.h3`
-  color: var(--gray-400);
+  color: var(--gray-700);
 
   margin: 0;
-  var(--gray-000)-space: nowrap;
+  white-space: nowrap;
   display: flex;
   align-items: center;
   justify-content: center;
