3817fbb6fd47427b49ffa9def418471ecf1cc8c4
simeunseo <rkdcjfehowl@naver.com>
2024-01-17 04:50:40 +0000

    fix: beneift img alt 태그 추가

--- src/@components/about/benefit/BenefitDesktop/BenefitItem.jsx
+++ src/@components/about/benefit/BenefitDesktop/BenefitItem.jsx
@@ -11,14 +11,14 @@ export default function BenefitItem({ imgSrc, iconSrc, bgColor, title, descripti
     <Link className={cx('benefitLink')} to={url} target="\_blank">
       <div className="d-flex flex-column">
         <div className={cx('benefitImgContainer', 'd-flex justify-content-center align-items-center', `${bgColor}`)}>
-          <img className={cx('benefitImg')} src={imgSrc} />
+          <img className={cx('benefitImg')} src={imgSrc} alt={title} />
         </div>
         <div className={cx('benefitContents', 'd-flex justify-content-between align-items-end')}>
           <div className={cx('benefitText', 'd-flex flex-column')}>
             <h3 className={cx('benefitTitle', 'w-100')}>{title}</h3>
             <p className={cx('benefitDescription', 'paragraph-lg w-100')}>{description}</p>
           </div>
-          <img className={cx('icon')} src={iconSrc} />
+          <img className={cx('icon')} src={iconSrc} alt={title} />
         </div>
       </div>
     </Link>
