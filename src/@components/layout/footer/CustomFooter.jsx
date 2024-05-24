73ff43fbf6762ebfa99d9487d0ce7dacaf69c190
eunbeann <ds5bean23@gmail.com>
2024-05-24 01:05:28 +0000

    fix: 링크 404 수정

--- src/@components/layout/footer/CustomFooter.jsx
+++ src/@components/layout/footer/CustomFooter.jsx
@@ -8,7 +8,7 @@ function CustomFooter() {
       <S.FooterContents>
         <S.FooterSlogan>Being All Seasons with goorm</S.FooterSlogan>
         <S.SocialContainer>
-          <S.SocialIcon href="https://github.com/goormthon-Univ" target="_blank" rel="noopener noreferrer">
+          <S.SocialIcon href="https://github.com/9oormthon-univ" target="_blank" rel="noopener noreferrer">
             <GithubIcon width="40" height="40" />
           </S.SocialIcon>
           <S.SocialIcon
@@ -18,9 +18,9 @@ function CustomFooter() {
           >
             <InstagramIcon width="40" height="40" />
           </S.SocialIcon>
-          <S.SocialIcon href="https://facebook.com/9oormthonuniv.official/" target="_blank" rel="noopener noreferrer">
+          {/*} <S.SocialIcon href="https://facebook.com/9oormthonuniv.official/" target="_blank" rel="noopener noreferrer">
             <FacebookIcon width="40" height="40" />
-          </S.SocialIcon>
+          </S.SocialIcon> */}
         </S.SocialContainer>
       </S.FooterContents>
     </S.FooterWrapper>
