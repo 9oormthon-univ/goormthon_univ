9d21d32822674b516b378a7db0bba0bd2e967118
seochan99 <gmlcks0513@naver.com>
2024-06-04 17:28:53 +0000

    feat : 2/3기 프로젝트 업데이트

--- src/@pages/project/Project.jsx
+++ src/@pages/project/Project.jsx
@@ -3,11 +3,13 @@ import * as S from './style';
 import SeasonImg from '../../assets/images/season.png';
 import ProjectCard from '../../@components/project/ProjectCard';
 import DanpoongProject2023Data from '../../utilities/ProjectData';
+import BeotkkotProject2024Data from '../../constants/BeotkkotProject2024Data';
+
 import { useNavigate } from 'react-router-dom';
 
 export default function Project() {
   const navigate = useNavigate();
-  const [activeIndex, setActiveIndex] = useState(0);
+  const [activeIndex, setActiveIndex] = useState(1);
   // Define the animation variants
   const fadeInVariants = {
     hidden: { opacity: 0 },
@@ -27,9 +29,9 @@ export default function Project() {
         <S.GoormBtn onClick={() => handleButtonClick(1)} active={activeIndex === 1}>
           2기
         </S.GoormBtn>
-        {/* <S.GoormBtn onClick={() => handleButtonClick(2)} active={activeIndex === 2}>
+        <S.GoormBtn onClick={() => handleButtonClick(2)} active={activeIndex === 2}>
           3기
-        </S.GoormBtn> */}
+        </S.GoormBtn>
       </S.BtnWrapper>
 
       <S.ProjectBody>
@@ -60,6 +62,39 @@ export default function Project() {
           </>
         )}
         {activeIndex === 1 && (
+          // <S.SecondTabContainer
+          //   initial="hidden"
+          //   animate="visible"
+          //   transition={{ duration: 1 }}
+          //   variants={fadeInVariants}
+          // >
+          //   <S.SeasonthonImg src={SeasonImg} />
+          //   <h4 style={{ color: '#2B2D36', marginTop: '1.5rem' }}>
+          //     구름톤 유니브와 함께하여 <br />
+          //     2기 갤러리를 꾸며주세요 :)
+          //   </h4>
+          //   <S.GoormBtn2 onClick={() => navigate('/recruit')}>모집 페이지로 가기</S.GoormBtn2>
+          // </S.SecondTabContainer>
+          <S.ProjectCardGrid>
+            {BeotkkotProject2024Data.map((project, index) => (
+              <ProjectCard
+                index={index}
+                award={project.award}
+                title={project.title}
+                content={project.content}
+                backendLink={project.backendLink}
+                frontendLink={project.frontendLink}
+                releaseLink={project.releaseLink}
+                image={project.image}
+                pm={project.pm}
+                design={project.design}
+                frontend={project.frontend}
+                backend={project.backend}
+              />
+            ))}
+          </S.ProjectCardGrid>
+        )}
+        {activeIndex === 2 && (
           <S.SecondTabContainer
             initial="hidden"
             animate="visible"
@@ -69,7 +104,7 @@ export default function Project() {
             <S.SeasonthonImg src={SeasonImg} />
             <h4 style={{ color: '#2B2D36', marginTop: '1.5rem' }}>
               구름톤 유니브와 함께하여 <br />
-              2기 갤러리를 꾸며주세요 :)
+              3기 갤러리를 꾸며주세요 :)
             </h4>
             <S.GoormBtn2 onClick={() => navigate('/recruit')}>모집 페이지로 가기</S.GoormBtn2>
           </S.SecondTabContainer>
