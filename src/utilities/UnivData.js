c95d7cc1af3ef5d9f078785db60fcc665ab86920
seochan99 <gmlcks0513@naver.com>
2024-06-10 09:11:13 +0000

    feat: 3기 대학 리스트 업데이트

--- src/utilities/UnivData.js
+++ src/utilities/UnivData.js
@@ -1,48 +1,38 @@
-import UnivImg1 from '../assets/images/univ/univ-ajou.png';
-import UnivImg2 from '../assets/images/univ/univ-cau.png';
-import UnivImg3 from '../assets/images/univ/univ-chonnam.png';
+import UnivImg1 from '../assets/images/univ/univ-kyunghee.png';
+import UnivImg2 from '../assets/images/univ/univ-korea.png';
+import UnivImg3 from '../assets/images/univ/univ-namseoul.png';
 import UnivImg4 from '../assets/images/univ/univ-dankook.png';
-import UnivImg5 from '../assets/images/univ/univ-dongduk.png';
+import UnivImg5 from '../assets/images/univ/univ-dongguk.png';
+import UnivImg6 from '../assets/images/univ/univ-samyuk.png';
+import UnivImg7 from '../assets/images/univ/univ-sangmyung(cheonan).png';
+import UnivImg8 from '../assets/images/univ/univ-seoulscience.png';
+import UnivImg9 from '../assets/images/univ/univ-sungkonghoe.png';
+import UnivImg10 from '../assets/images/univ/univ-sch.png';
 
-import UnivImg6 from '../assets/images/univ/univ-dongguk.png';
-import UnivImg7 from '../assets/images/univ/univ-duksung.png';
-import UnivImg8 from '../assets/images/univ/univ-ewha.png';
-import UnivImg9 from '../assets/images/univ/univ-gachon.png';
-import UnivImg10 from '../assets/images/univ/univ-hallym.png';
-
-import UnivImg11 from '../assets/images/univ/univ-hansung.png';
-import UnivImg12 from '../assets/images/univ/univ-HUFS.png';
+import UnivImg11 from '../assets/images/univ/univ-ssu.png';
+import UnivImg12 from '../assets/images/univ/univ-ajou.png';
 import UnivImg13 from '../assets/images/univ/univ-incheon.png';
-import UnivImg14 from '../assets/images/univ/univ-kookmin.png';
-import UnivImg15 from '../assets/images/univ/univ-korea.png';
-
-import UnivImg16 from '../assets/images/univ/univ-kwangwoon.png';
-import UnivImg17 from '../assets/images/univ/univ-kyonggi.png';
-import UnivImg18 from '../assets/images/univ/univ-kyunghee.png';
-import UnivImg19 from '../assets/images/univ/univ-sangmyung(cheonan).png';
-import UnivImg20 from '../assets/images/univ/univ-sangmyung(seoul).png';
-
-import UnivImg21 from '../assets/images/univ/univ-sch.png';
-import UnivImg22 from '../assets/images/univ/univ-sehan.png';
-import UnivImg23 from '../assets/images/univ/univ-sejong.png';
-import UnivImg24 from '../assets/images/univ/univ-sungkonghoe.png';
-import UnivImg25 from '../assets/images/univ/univ-swu.png';
+import UnivImg14 from '../assets/images/univ/univ-inha.png';
+import UnivImg15 from '../assets/images/univ/univ-chonnam.png';
+import UnivImg16 from '../assets/images/univ/univ-hankukengineering.png';
+import UnivImg17 from '../assets/images/univ/univ-HUFS.png';
+import UnivImg18 from '../assets/images/univ/univ-hansung.png';
 
 const Universities = [
   {
     image: UnivImg1,
-    name: '아주대학교',
-    link: 'https://www.instagram.com/9oormthon_univ_ajou/',
+    name: '경희대학교(국제)',
+    link: 'https://www.instagram.com/9oormthon_univ_khu/',
   },
   {
     image: UnivImg2,
-    name: '중앙대학교',
-    link: 'https://www.instagram.com/9oormthon_univ_cau/',
+    name: '고려대학교(세종)',
+    link: 'https://www.instagram.com/9oormthon_univ_kus/',
   },
   {
     image: UnivImg3,
-    name: '전남대학교',
-    link: 'https://www.instagram.com/9oormthon_univ_jnu/',
+    name: '남서울대학교',
+    link: 'https://www.instagram.com/9oormthonuniv.official',
   },
   {
     image: UnivImg4,
@@ -51,108 +41,73 @@ const Universities = [
   },
   {
     image: UnivImg5,
-    name: '동덕대학교',
-    link: 'https://www.instagram.com/9oormthon_univ_ddwu/',
-  },
-  {
-    image: UnivImg6,
     name: '동국대학교',
     link: 'https://www.instagram.com/9oormthon.univ.dgu/',
   },
   {
+    image: UnivImg6,
+    name: '삼육대학교',
+    link: 'https://www.instagram.com/9oormthonuniv.official',
+  },
+  {
     image: UnivImg7,
-    name: '덕성대학교',
-    link: 'https://www.instagram.com/9oormthon_univ_dswu/',
-  },
-  {
-    image: UnivImg8,
-    name: '이화여자대학교',
-    link: 'https://www.instagram.com/9oormthon_univ_ewha/',
-  },
-  {
-    image: UnivImg9,
-    name: '가천대학교',
-    link: 'https://www.instagram.com/9oormthon_univ_gcu/',
-  },
-  {
-    image: UnivImg10,
-    name: '한림대학교',
-    link: 'https://www.instagram.com/9oormthon_univ_hlu/',
-  },
-  {
-    image: UnivImg11,
-    name: '한성대학교',
-    link: 'https://www.instagram.com/9oormthon_univ_hsu/',
-  },
-  {
-    image: UnivImg12,
-    name: '한국외국어대학교(글로벌)',
-    link: 'https://www.instagram.com/9oormthon_univ_hufs/',
-  },
-  {
-    image: UnivImg13,
-    name: '인천대학교',
-    link: 'https://www.instagram.com/9oormthon_univ_inu/',
-  },
-  {
-    image: UnivImg14,
-    name: '국민대학교',
-    link: 'https://www.instagram.com/9oormthon_univ_kmu/',
-  },
-  {
-    image: UnivImg15,
-    name: '고려대학교(세종)',
-    link: 'https://www.instagram.com/9oormthon_univ_kus/',
-  },
-  {
-    image: UnivImg16,
-    name: '광운대학교',
-    link: 'https://www.instagram.com/9oormthon_univ_kwu/',
-  },
-  {
-    image: UnivImg17,
-    name: '경기대학교',
-    link: 'https://www.instagram.com/9oormthon_univ_kgu/',
-  },
-  {
-    image: UnivImg18,
-    name: '경희대학교',
-    link: 'https://www.instagram.com/9oormthon_univ_khu/',
-  },
-  {
-    image: UnivImg19,
     name: '상명대학교(천안)',
     link: 'https://www.instagram.com/9oormthon_univ_smuc/',
   },
   {
-    image: UnivImg20,
-    name: '상명대학교(서울)',
-    link: 'https://www.instagram.com/9oormthon_univ_smu/',
+    image: UnivImg8,
+    name: '서울과학기술대학교',
+    link: 'https://www.instagram.com/9oormthonuniv.official',
   },
   {
-    image: UnivImg21,
-    name: '순천향대학교',
-    link: 'https://www.instagram.com/9oormthon_univ_schu/',
-  },
-  {
-    image: UnivImg22,
-    name: '세한대학교(당진)',
-    link: 'https://www.instagram.com/9oormthon_univ_shu/',
-  },
-  {
-    image: UnivImg23,
-    name: '세종대학교',
-    link: 'https://www.instagram.com/9oormthon_univ_sju/',
-  },
-  {
-    image: UnivImg24,
+    image: UnivImg9,
     name: '성공회대학교',
     link: 'https://www.instagram.com/9oormthon_univ_skhu/',
   },
   {
-    image: UnivImg25,
-    name: '서울여자대학교',
-    link: 'https://www.instagram.com/9oormthon_univ_swu/',
+    image: UnivImg10,
+    name: '순천향대학교',
+    link: 'https://www.instagram.com/9oormthon_univ_schu/',
+  },
+  {
+    image: UnivImg11,
+    name: '숭실대학교',
+    link: 'https://www.instagram.com/9oormthonuniv.official',
+  },
+  {
+    image: UnivImg12,
+    name: '아주대학교',
+    link: 'https://www.instagram.com/9oormthon_univ_ajou/',
+  },
+  {
+    image: UnivImg13,
+    name: '인천대학교',
+    link: 'https://www.instagram.com/9oormthonuniv.inu/',
+  },
+  {
+    image: UnivImg14,
+    name: '인하대학교',
+    link: 'https://www.instagram.com/9oormthonuniv.official',
+  },
+  {
+    image: UnivImg15,
+    name: '전남대학교',
+    link: 'https://www.instagram.com/9oormthon_univ_jnu/',
+  },
+  {
+    image: UnivImg16,
+    name: '한국공학대학교',
+    link: 'https://www.instagram.com/9oormthonuniv.official',
+  },
+  {
+    image: UnivImg17,
+    name: '한국외국어대학교(글로벌)',
+    link: 'https://www.instagram.com/9oormthon_univ_hufs/',
+  },
+  {
+    image: UnivImg18,
+    name: '한성대학교',
+    link: 'https://www.instagram.com/9oormthon_univ_hsu/',
   },
 ];
 
